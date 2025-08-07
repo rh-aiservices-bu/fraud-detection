bd1665607086dae4656ade00a3a54a78ade6546c
jyejare <jyejare@redhat.com>
2025-08-07 19:28:14 +0530

    Fraud Detection features using Feast
    Signed-off-by: jyejare <jyejare@redhat.com>

--- feast/feature_repo/store_objects.py
+++ feast/feature_repo/store_objects.py
@@ -6,7 +6,7 @@ enty = Entity(name='entity_id', join_keys=['entity_id'])
 
 train_source = FileSource(
     name="train_data_source",
-    path="data/train.parquet",
+    path="/feast-data/fraud_detect/data/train.parquet",
     timestamp_field='event_timestamp',
     created_timestamp_column='created_timestamp'
 )
@@ -14,7 +14,7 @@ train_push = PushSource(name='train_push_source', batch_source=train_source)
 
 validate_source = FileSource(
     name="validate_data_source",
-    path="data/validate.parquet",
+    path="/feast-data/fraud_detect/data/validate.parquet",
     timestamp_field='event_timestamp',
     created_timestamp_column='created_timestamp'
 )
@@ -22,18 +22,17 @@ validate_push = PushSource(name='validate_push_source', batch_source=validate_so
 
 test_source = FileSource(
     name="test_data_source",
-    path="data/test.parquet",
+    path="/feast-data/fraud_detect/data/test.parquet",
     timestamp_field='event_timestamp',
     created_timestamp_column='created_timestamp'
 )
 test_push = PushSource(name='test_push_source', batch_source=test_source)
 
 train_view = FeatureView(
-    name="next_train_view",
+    name="fraud_detection_train_view",
     entities=[enty],
     ttl=timedelta(days=1),
     schema=[
-        Field(name="entity_id", dtype=Int64),
         Field(name='distance_from_last_transaction', dtype=Float64),
         Field(name='ratio_to_median_purchase_price', dtype=Float64),
         Field(name="used_chip", dtype=Float32),
